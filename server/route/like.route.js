ef04dd37730017dea07e7961296120b22fe82a58
borsejugal23 <115457172+borsejugal23@users.noreply.github.com>
2024-02-12 10:47:08 +0530

    comment added

--- server/route/like.route.js
+++ server/route/like.route.js
@@ -8,7 +8,6 @@ const likeRoute = express.Router();
 
 // const axios= require("axios")
 
-
 likeRoute.post("/:productId/:userId", async (req, res) => {
   const { productId, userId } = req.params;
   const { liked } = req.body;
@@ -27,127 +26,61 @@ likeRoute.post("/:productId/:userId", async (req, res) => {
   });
 });
 
+likeRoute.get("/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    console.log(userId);
 
-
-likeRoute.get("/:userId", async(req,res)=>{
-    try {
-
-        const userId= req.params.userId;
-        console.log(userId)
-
-        let likesAggregation = await Like.aggregate([
-            {
-              $group: {
-                _id: "$productId",
-                totalLikes: {
-                  $sum: {
-                    $cond: [{ $eq: ["$liked", true] }, 1, 0],
-                  },
-                },
-                totalDislikes: {
-                  $sum: {
-                    $cond: [{ $eq: ["$liked", false] }, 1, 0],
-                  },
-                },
-              },
+    let likesAggregation = await Like.aggregate([
+      {
+        $group: {
+          _id: "$productId",
+          totalLikes: {
+            $sum: {
+              $cond: [{ $eq: ["$liked", true] }, 1, 0],
             },
-            {
-              $project: {
-                _id: 0,
-                productId: "$_id",
-                totalLikes: 1,
-                totalDislikes: 1,
-              },
+          },
+          totalDislikes: {
+            $sum: {
+              $cond: [{ $eq: ["$liked", false] }, 1, 0],
             },
-          ]);
-      
-          const userAllLikes= await Like.find({userId :userId}) ;
-         
-      
-  
-          // console.log(likesAggregation);
-          const allLikes = likesAggregation.map((likedData) => { //0
+          },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          productId: "$_id",
+          totalLikes: 1,
+          totalDislikes: 1,
+        },
+      },
+    ]);
 
-              const userLiked = userAllLikes.find((like) => like.productId === likedData.productId);
+    const userAllLikes = await Like.find({ userId: userId });
 
-              return userLiked   ? { ...likedData, liked: userLiked.liked }
-                                                                                  : { ...likedData, liked: false };
-            });
-          
+    // console.log(likesAggregation);
+    const allLikes = likesAggregation.map((likedData) => {
+      //0
 
-            res.status(200).
-            send(
-                {allLikes: allLikes}) 
-    
-    } catch (error) {
-        console.log("error In likes : ", error.message)
-        res.status(500).send({message:"error in fetching the Likes"})
-    }
-})
+      const userLiked = userAllLikes.find(
+        (like) => like.productId === likedData.productId
+      );
 
+      return userLiked
+        ? { ...likedData, liked: userLiked.liked }
+        : { ...likedData, liked: false };
+    });
 
-console.log("         ==========================================================================   ")
-// async function getLikes() {
+    res.status(200).send({ allLikes: allLikes });
+  } catch (error) {
+    console.log("error In likes : ", error.message);
+    res.status(500).send({ message: "error in fetching the Likes" });
+  }
+});
 
-//     const {category} = {category :""}
-//     const response = await axios.get(`https://dummyjson.com/products/${category ? `category/${category}` : ""}`);
-    
-
-//     const data = response.data;
-//     const products= data.products ;
-
-
-//     console.log(products);
-//     let likesAggregation = await Like.aggregate([
-//       {
-//         $group: {
-//           _id: "$productId",
-//           totalLikes: {
-//             $sum: {
-//               $cond: [{ $eq: ["$liked", true] }, 1, 0],
-//             },
-//           },
-//           totalDislikes: {
-//             $sum: {
-//               $cond: [{ $eq: ["$liked", false] }, 1, 0],
-//             },
-//           },
-//         },
-//       },
-//       {
-//         $project: {
-//           _id: 0,
-//           productId: "$_id",
-//           totalLikes: 1,
-//           totalDislikes: 1,
-//         },
-//       },
-//     ]);
-
-//     const likes= await Like.find({userId :"65c78286e82ef6a0e2c020b1"}) ;
-   
-
-  
-//     // console.log(likesAggregation);
-//     const mergedData = likesAggregation.map((likedData) => {
-//         const matchingLike = likes.find((like) => like.productId === likedData.productId);
-//         return matchingLike
-//           ? { ...likedData, liked: matchingLike.liked }
-//           : { ...likedData, liked: false };
-//       });
-
-
-
-
-      
-//     //   console.log(mergedProducts);
-      
-
-//   }
-  
-//   getLikes();
-  
-  
+console.log("    ==========================================================================   "
+);
 
 
 module.exports = { likeRoute };
--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,82 +1,94 @@
-const express=require("express");
-const userRouter=express.Router();
-const bcrypt=require("bcrypt");
-const jwt=require("jsonwebtoken");
-require("dotenv").config()
+const express = require("express");
+const userRouter = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 const { userModel } = require("../model/user.model");
 
+userRouter.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-userRouter.post("/signup",async(req,res)=>{
-   try {
-    const {name,email,password}=req.body;
+    // Check for missing fields
+    const missingFields = [];
+    if (!name) missingFields.push("name");
+    if (!email) missingFields.push("email");
+    if (!password) missingFields.push("password");
 
-     // Check for missing fields
+    // If any fields are missing, return an error
+    if (missingFields.length > 0) {
+      return res
+        .status(400)
+        .json({
+          msg: `Please fill in the following fields: ${missingFields.join(
+            ","
+          )}`,
+        });
+    }
+    const existinguser = await userModel.find({ email });
+
+    if (existinguser.length) {
+      return res
+        .status(201)
+        .json({ msg: "User has already registered,please login" });
+    }
+
+    bcrypt.hash(password, 5, async (err, hash) => {
+      if (err) {
+        return res.status(400).json({ msg: err.message });
+      } else {
+        const newuser = new userModel({ name, email, password: hash });
+        await newuser.save();
+        return res.status(200).json({ msg: "Registered successfully" });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
   const missingFields = [];
-  if (!name) missingFields.push('name');
-  if (!email) missingFields.push('email');
-  if (!password) missingFields.push('password');
+  if (!email) missingFields.push("email");
+  if (!password) missingFields.push("password");
 
   // If any fields are missing, return an error
   if (missingFields.length > 0) {
-    return res.status(400).json({ msg: `Please fill in the following fields: ${missingFields.join(',')}` });
+    return res
+      .status(200)
+      .json({
+        msg: `Please fill in the following fields: ${missingFields.join(",")}`,
+      });
   }
-    const existinguser= await userModel.find({email});
-
-    if (existinguser.length){
-        return res.status(201).json({msg:"User has already registered,please login"})
+  try {
+    const existinguser = await userModel.findOne({ email });
+    if (!existinguser) {
+      return res
+        .status(404)
+        .json({ msg: "User not found,please create account" });
     }
+    bcrypt.compare(password, existinguser.password, async (err, result) => {
+      if (result) {
+        const token = jwt.sign(
+          { userID: existinguser._id, username: existinguser.name },
+          process.env.secretKey
+        );
+        return res
+          .status(200)
+          .json({
+            msg: "Login successfully",
+            token: token,
+            userId: existinguser._id,
+          });
+      } else {
+        return res.status(400).json({ msg: "Wrong credential" });
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-    bcrypt.hash(password,5,async(err,hash)=>{
-
-        if (err){
-            return res.status(400).json({msg:err.message})
-        }
-        else{
-            const newuser= new userModel({name,email,password:hash});
-            await newuser.save();
-            return res.status(200).json({msg:"Registered successfully"})
-        }
-    })
-
-
-
-   } catch (error) {
-    res.status(500).json({error:error.message})
-   }
-})
-
-
-userRouter.post("/login",async(req,res)=>{
-    const {email,password}=req.body;
-
-    const missingFields = [];
-    if (!email) missingFields.push('email');
-    if (!password) missingFields.push('password');
-  
-    // If any fields are missing, return an error
-    if (missingFields.length > 0) {
-      return res.status(200).json({ msg: `Please fill in the following fields: ${missingFields.join(',')}` });
-    }
-    try {
-        const existinguser= await userModel.findOne({email});
-        if (!existinguser){
-           
-            return res.status(404).json({msg:"User not found,please create account"})
-        }
-        bcrypt.compare(password,existinguser.password,async(err,result)=>{
-            if (result){
-                const token=jwt.sign({userID:existinguser._id,username:existinguser.name},process.env.secretKey);
-                return res.status(200).json({msg:"Login successfully",token:token,userId:existinguser._id})
-
-            }
-            else{
-                return res.status(400).json({msg:"Wrong credential"})
-            }
-        })
-    
-    } catch (error) {
-     res.status(500).json({error:error.message})
-    }
-})
-
-module.exports={userRouter}
+module.exports = { userRouter };
