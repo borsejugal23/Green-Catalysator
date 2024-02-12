73411cde1853cececef583d8706cbaf56793d63e
borsejugal23 <115457172+borsejugal23@users.noreply.github.com>
2024-02-12 09:20:37 +0530

    footer is updated

--- client/src/Pages/Footer.jsx
+++ client/src/Pages/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <>
       <br />
       <br />
-      <footer className="bg-gray-800 text-white py-4 fixed bottom-0 w-full">
+      <footer className="bg-gray-800 text-white py-4  w-full">
         <div className="container mx-auto text-center">
           <p className="text-md">
             &copy; {new Date().getFullYear()} GREEN CATALYSATOR IT AB. All rights
